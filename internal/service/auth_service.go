120792b729d1fe4c4cafa7764e854953ddfb6ecb
Rashomon <cn475@icloud.com>
2026-09-08 11:16:34 +0900

    feat: add username validation in Register

--- internal/service/auth_service.go
+++ internal/service/auth_service.go
@@ -3,6 +3,8 @@ package service
 import (
 	"errors"
 	"fmt"
+	"strings"
+	"unicode"
 
 	"github.com/Rashomon-code/myblog/internal/model"
 	"golang.org/x/crypto/bcrypt"
@@ -22,7 +24,33 @@ func NewAuthService(repo AuthRepositoryInterface, jwt *JWTService) *AuthService 
 	return &AuthService{repo: repo, jwt: jwt}
 }
 
+// 正規表現で制限する方法
+// var usernameRegex = regexp.MustCompile(`^[a-zA-Z0-9_]{3,20}$`)
+// func validateUsernameFormat(username string) error {
+// 	if !usernameRegex.MatchString(username) {
+// 		return errors.New("Format error")
+// 	}
+// 	return nil
+// }
+
+func validateUsername(username string) error {
+	if len(username) < 3 || len(username) > 20 {
+		return errors.New("3 文字から 20 文字を入力してください")
+	}
+
+	if strings.IndexFunc(username, unicode.IsSpace) != -1 {
+		return errors.New("スペースが含まれています")
+	}
+
+	return nil
+}
+
 func (s *AuthService) Register(username, password string) error {
+	err := validateUsername(username)
+	if err != nil {
+		return err
+	}
+
 	passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), 4) //学習のため、最低レベルを使用します
 	if err != nil {
 		return fmt.Errorf("登録できませんでした: %w", err)
